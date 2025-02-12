0690d79a57e3df3654b72232b2a72c31245f5cfa
iamanastasi <a1301148@gmail.com>
2025-02-12 23:42:59 +0300

    it works!

--- sqlite/demo.js
+++ sqlite/demo.js
@@ -2,22 +2,22 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // Создаем или открываем базу данных
-const db = new sqlite3.Database('PMSdemo.db', (err) => {
+const db = new sqlite3.Database('mydatabase.db', (err) => {
     if (err) {
         console.error(err.message);
     }
-    console.log('Connected to the PMSdemo.db SQlite database.');
+    console.log('Connected to the mydatabase.db SQlite database.');
 });
 
-// Создаем таблицу
+
 db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS users (
+    db.run(`CREATE TABLE IF NOT EXISTS tenants (
         TenantID INTEGER PRIMARY KEY AUTOINCREMENT,
         SecondName TEXT NOT NULL,
         FirstName TEXT NOT NULL,
         ThirdName TEXT,
         email TEXT NOT NULL UNIQUE,
-        telephone TEXT NOT NULL UNIQUE,
+        telephone TEXT NOT NULL,
         TenantNumber INTEGER
 
     )`, (err) => {
@@ -27,17 +27,17 @@ db.serialize(() => {
     });
 
     // Вставляем данные
-    const stmt = db.prepare(`INSERT INTO tenants ( SecondName, FirstName, ThirdName, email, telephone, TenantNumber VALUES (?, ?)`);
+    const stmt = db.prepare(`INSERT INTO tenants (SecondName, FirstName, ThirdName, email, telephone, TenantNumber) VALUES (?, ?, ?, ?, ?, ?)`);
     stmt.run('Scufsky', 'Milf', 'Hunter', 'lol@example.com', '7952812', 1);
-    stmt.run('Ikari', 'Shinji', 'AAAAAA', 'kek@example.com', '7952812', 52);
+    stmt.run('Ikari', 'Shinji', 'AAAAAA', 'kek@example.com', '79528126969', 52);
     stmt.finalize();
 
     // Читаем данные
-    db.each(`SELECT id, name, email FROM users`, (err, row) => {
+    db.each(`SELECT TenantID, SecondName, FirstName, ThirdName, email, telephone, TenantNumber FROM tenants`, (err, row) => {
         if (err) {
             console.error(err.message);
         }
-        console.log(`${row.id}: ${row.name}, ${row.email}`);
+        console.log(`${row.TenantID}: ${row.SecondName}, ${row.FirstName}, ${row.ThirdName}, ${row.email}, ${row.telephone}, ${row.TenantNumber}`);
     });
 });
 

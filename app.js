c9efa2a63399c23749441c9395531bcd861c7196
iamanastasi <a1301148@gmail.com>
2025-02-18 09:30:02 +0300

    .

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
+function printTable(){
 let currentDate = new Date();
-console.log(currentDate.getFullYear());
-console.log(currentDate.getMonth());
-console.log(currentDate.getDate());
-console.log(currentDate);
 const MonthArr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-today=currentDate.getDate();
-tomonth=currentDate.getMonth();
-count =1;
-for(let i=today; i<MonthArr[tomonth]; i++)
+const MonthArrStr = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+const today = currentDate.getDate();
+const month = currentDate.getMonth();
+const month1 = document.getElementById('month1');
+month1.textContent = MonthArrStr[month];
+month1.colSpan = MonthArr[month]-today +1;
+const month2 = document.getElementById('month2');
+month2.textContent = MonthArrStr[month+1];
+month2.colSpan = 31-MonthArr[month]+today;
+let count=1;
+   let headerCount = 1;
+for (let i = today; i <= MonthArr[month]; i++) {
+    const hdayElements = document.querySelectorAll('th.day' + count);
+    hdayElements[0].textContent = i;
+    count++;
+}
+for(let i=1; i<=MonthArr[month+1]&&count<=30; i++)
 {
-document.getElementById('day'+count.toString()).textContent= i;
-count++;
+  const hdayElements = document.querySelectorAll('th.day' + count);
+  hdayElements[0].textContent = i; 
+  count++;
+}
+const trCount = document.querySelectorAll('tbody tr').length;
+for (let j = 1; j <= trCount; j++) {
+    count = 1; 
+    for (let i = today; i <= MonthArr[month]; i++) {
+        const ddayElements = document.querySelectorAll('tr.row' + j + ' td.day' + count);
+        ddayElements[0].textContent = i;
+        count++;
+    }
+}
+let countend=count;
+    for (let j = 1; j <= trCount; j++) {
+    count = countend;
+    for (let i = 1; i <= MonthArr[month + 1] && count <= 30; i++) {
+        const ddayElementsNextMonth = document.querySelectorAll('tr.row' + j + ' td.day' + count);
+        ddayElementsNextMonth[0].textContent = i;
+        count++;
+    }
+}
 }
